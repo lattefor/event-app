6111b8e921de28833ea06cd164006d273d6eaf24
mitsuko yoneyama <latteforever@gmail.com>
2024-06-01 15:55:51 -0700

    feature complete

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
 import { SearchParamProps } from "@/types";
 
 import { auth } from '@clerk/nextjs'
@@ -16,9 +18,9 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
     const ordersPage = Number(searchParams?.ordersPage) || 1;
     const eventsPage = Number(searchParams?.eventsPage) || 1;
   
-    ///const orders = await getOrdersByUser({ userId, page: ordersPage})
+    const orders = await getOrdersByUser({ userId, page: ordersPage})
   
-    /// const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
     const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
   
 
@@ -34,7 +36,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
@@ -45,7 +47,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
         />
-      </section> */}
+      </section>
 
       {/* Event Organized */}
 
--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { Button } from '../ui/button'
@@ -10,8 +12,44 @@ type PaginationProps = {
 }
 
 const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === 'next' 
+      ? Number(page) + 1 
+      : Number(page) - 1
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
+    })
+
+    router.push(newUrl, {scroll: false})
+  }
+
   return (
-    <div>Pagination</div>
+    <div className="flex gap-2">
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('prev')}
+        disabled={Number(page) <= 1}
+      >
+        Previous
+      </Button>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        onClick={() => onClick('next')}
+        disabled={Number(page) >= totalPages}
+      >
+        Next
+      </Button>
+    </div>
   )
 }
 
