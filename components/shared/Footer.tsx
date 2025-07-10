653b68cd2b90deb74d3d2ba5741306150965651b
mitsuko yoneyama <latteforever@gmail.com>
2025-07-10 13:50:32 -0700

    feature: add gradient to header footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { AnimatedGradient } from '../ui/animated-gradient-with-svg'
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+    <footer className="relative border-t">
+      <AnimatedGradient
+        colors={["#EC4899", "#F472B6", "#3B82F6"]}
+        speed={0.08}
+        blur="light"
+      />
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row relative z-10 backdrop-blur-sm">
         <Link href='/'>
           <Image 
             src="/assets/images/logo.png"
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,13 +3,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from "../ui/button"
+import { AnimatedGradient } from "../ui/animated-gradient-with-svg"
 import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
-    <header className="w-full border-b">
-      <div className="wrapper flex items-center justify-between">
+    <header className="relative w-full border-b">
+      <AnimatedGradient
+        colors={["#3B82F6", "#93C5FD", "#c12fde" ]}
+        speed={0.1}
+        blur="light"
+      />
+      <div className="wrapper flex items-center justify-between relative z-10">
         <Link href="/" className="w-36">
           <Image 
             src="/assets/images/logo.png" width={128} height={38}
